c01f322b13e17bd3ef33bcdbc6609b9189ed89a0
NAVEEN-M-J <46046229+NAVEEN-M-J@users.noreply.github.com>
2018-12-21 05:13:08 -0500

    Add files via upload
    PROJECT Valaxy Technologies Devops

---
+++ maven-hello-world-master/maven-hello-world-master/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,29 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.out.println("A NullPointerException is a drag...");
+        Object myObject = null;
+        /**
+         * Checker prevents this from compiling...
+         */
+//        System.out.println("myObject: " + myObject.toString());
+        /**
+         * ... which is simply fantastic. It shows:
+         *
+         * error: [dereference.of.nullable] dereference of possibly-null
+         * reference myObject
+         *
+         * http://checkerframework.org
+         */
+        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+    }
+}
